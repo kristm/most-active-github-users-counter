60eaf2d8d1c78aa31f031a30479e8a82047166bf
Krist Menina <krist@quipper.com>
2018-02-03 15:23:47 +0800

    add quipper location presets

--- presets.go
+++ presets.go
@@ -3,7 +3,13 @@ package main
 type PresetLocations []string
 
 var PRESETS = map[string]PresetLocations{
-  "philippines": PresetLocations{"ph", "philippines", "filipinas", "pilipinas", "manila", "makati", "cebu", "davao", "bohol", "bacolod", "iloilo", "baguio", "vigan"}}
+  "default": PresetLocations{"id", "indonesia", "jp", "japan", "mx", "mexico", "ph", "philippines", "uk", "london"},
+  "indonesia": PresetLocations{"id", "indonesia", "jakarta", "bandung", "makassar", "java", "surabaya", "bekasi", "sumedang"},
+  "japan":PresetLocations{"jp", "japan", "tokyo", "yokohama", "osaka", "nagoya", "sapporo", "kobe", "kyoto", "fukuoka", "kawasaki", "saitama", "hiroshima", "sendai"},
+  "mexico": PresetLocations{"mx", "mexico", "méxico", "guadalajara", "chihuahua", "juarez", "cancun", "mexicali", "veracruz", "oaxaca"},
+  "philippines": PresetLocations{"ph", "philippines", "filipinas", "pilipinas", "manila", "makati", "cebu", "davao", "bohol", "bacolod", "iloilo", "baguio", "vigan"},
+  "uk": PresetLocations{"uk","london","birmingham","leeds","glasgow","sheffield","bradford","manchester","edinburgh","liverpool","bristol","cardiff","belfast","leicester","wakefield","coventry","nottingham","newcastle"},
+}
 
 func Preset(name string) []string {
   return PRESETS[name]
