c7554a6aa7c4b3bb939e542e967c02038b7ea8d7
Krist Menina <krist@quipper.com>
2018-02-09 19:02:26 +0800

    remove accent character. it breaks github api query

--- presets.go
+++ presets.go
@@ -6,7 +6,7 @@ var PRESETS = map[string]PresetLocations{
   "default": PresetLocations{"id", "indonesia", "jp", "japan", "mx", "mexico", "ph", "philippines", "uk", "london"},
   "id": PresetLocations{"id", "indonesia", "jakarta", "bandung", "makassar", "java", "surabaya", "bekasi", "sumedang"},
   "jp":PresetLocations{"jp", "japan", "tokyo", "yokohama", "osaka", "nagoya", "sapporo", "kobe", "kyoto", "fukuoka", "kawasaki", "saitama", "hiroshima", "sendai"},
-  "mx": PresetLocations{"mx", "mexico", "méxico", "guadalajara", "chihuahua", "juarez", "cancun", "mexicali", "veracruz", "oaxaca"},
+  "mx": PresetLocations{"mx", "mexico", "guadalajara", "chihuahua", "juarez", "cancun", "mexicali", "veracruz", "oaxaca"},
   "ph": PresetLocations{"ph", "philippines", "filipinas", "pilipinas", "manila", "makati", "cebu", "davao", "bohol", "bacolod", "iloilo", "baguio", "vigan"},
   "uk": PresetLocations{"uk","london","birmingham","leeds","glasgow","sheffield","bradford","manchester","edinburgh","liverpool","bristol","cardiff","belfast","leicester","wakefield","coventry","nottingham","newcastle"},
 }
