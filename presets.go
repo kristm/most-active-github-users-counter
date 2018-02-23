3476f6a1404fc7c8b0a5564d0c398a17ea0f17d6
Krist Menina <krist@quipper.com>
2018-02-23 13:05:50 +0800

    add id city

--- presets.go
+++ presets.go
@@ -4,7 +4,7 @@ type PresetLocations []string
 
 var PRESETS = map[string]PresetLocations{
   "default": PresetLocations{"id", "indonesia", "jp", "japan", "mx", "mexico", "ph", "philippines", "uk", "london"},
-  "id": PresetLocations{"id", "indonesia", "jakarta", "bandung", "makassar", "java", "surabaya", "bekasi", "sumedang"},
+  "id": PresetLocations{"id", "indonesia", "jakarta", "bandung", "banjarmasin", "makassar", "java", "surabaya", "bekasi", "sumedang"},
   "jp":PresetLocations{"jp", "japan", "tokyo", "yokohama", "osaka", "nagoya", "sapporo", "kobe", "kyoto", "fukuoka", "kawasaki", "saitama", "hiroshima", "sendai"},
   "mx": PresetLocations{"mx", "mexico", "guadalajara", "chihuahua", "juarez", "cancun", "mexicali", "veracruz", "oaxaca"},
   "ph": PresetLocations{"ph", "philippines", "filipinas", "pilipinas", "manila", "makati", "cebu", "davao", "bohol", "bacolod", "iloilo", "baguio", "vigan"},
